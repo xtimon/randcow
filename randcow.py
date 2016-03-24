a748e3c39fd277a2c77f6638dd9d3da13124d3c1
Тимур Исанов <timur.isanov@bars-open.ru>
2016-03-24 18:31:28 +0300

    first commit

---
+++ randcow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+import os
+import random
+import subprocess
+import feedparser
+
+cows_path = '/usr/share/cowsay/cows/'
+rss_feed = 'http://www.anekdot.ru/rss/export20.xml'
+
+cows_list = os.listdir(cows_path)
+feed = feedparser.parse(rss_feed)
+cow = random.choice(cows_list)
+msg = random.choice(feed.entries).description
+subprocess.call("cowsay -f {0} \"{1}\"".format(cow, msg), shell=True)
